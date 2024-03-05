9925c2754bb92e67886a83771ca67745119d6420
Bruno Tohoru Shiotani <bruno_shiotani@outlook.com>
2024-03-05 11:45:15 -0300

    S6 - A36 Navegação entre rotas

--- app_super_gestao/app/Http/Controllers/ContatoController.php
+++ app_super_gestao/app/Http/Controllers/ContatoController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class ContatoController extends Controller
 {
     public function contato(){
-        echo 'Faça contato conosco';
+       return view('site.contato');
     }
 }
--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -17,26 +17,3 @@ Route::get('/', 'PrincipalController@principal');
 Route::get('/sobre-nos', 'SobreNosController@sobreNos');
 
 Route::get('/contato', 'ContatoController@contato');
-//nome, categoria, assunto, mensagem
-
-Route::get(
-    '/contato/{nome}/{categoria_id}', 
-    function(
-        string $nome = 'Desconhecido',
-        int $categoria_id = 1 // 1 - Informação 
-    ){
-        echo "Estamos aqui: $nome - $categoria_id";
-    }
-)->where('categoria_id','[0-9]+')//[0-9]+ -> caracteres entre 0 e 9 e tem que ter pelo menos 1 caracter.
-->where('nome', '[A-Za-z]+')//[A-Za-z]+ -> entre A e Z e entre a e z contendo pelo menos 1 caracter. 
-;
-/* verbo http
-
-get
-post
-put
-patch
-delete
-options
-
-*/
