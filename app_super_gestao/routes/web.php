85784f6a33dd3a2c33e27da69afc5fa51ced3936
Bruno Tohoru Shiotani <bruno_shiotani@outlook.com>
2024-03-05 11:57:06 -0300

    S6-A38

--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -13,15 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::get('/', 'PrincipalController@principal');
-
 Route::get('/sobre-nos', 'SobreNosController@sobreNos');
-
 Route::get('/contato', 'ContatoController@contato');
-
 Route::get('/login', function(){ return 'Login';});
 
+//agrupamento de rotas
+Route::prefix('/app')->group(function(){
 Route::get('/clientes', function(){ return 'Clientes';});
-
 Route::get('/fornecedores', function(){ return 'Fornecedores';});
-
 Route::get('/produtos', function(){ return 'Produtos';});
+});
