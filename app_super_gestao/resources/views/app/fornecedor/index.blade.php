9f8ec962a402e4088a51c706a15d346611a36290
Bruno Tohoru Shiotani <bruno_shiotani@outlook.com>
2024-03-06 10:43:16 -0300

    S7 - A44 - Blade incluindo comentários e PHP puro

---
+++ app_super_gestao/resources/views/app/fornecedor/index.blade.php
@@ -0,0 +1,15 @@
+<h3>Fornecedor</h3>
+
+{{-- Fica o comentario que será descartado pelo interpretador do BLADE --}}
+
+{{'Texto de teste'}}
+
+<?= 'Texto de teste'?>
+
+@php
+    //Para comentarios de uma linha
+    /*
+    Para comentarios em bloco
+    */
+    echo 'teste'
+@endphp
