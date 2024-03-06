5117d42b3de3548bcd9a109c13369f9c73185bc2
Bruno Tohoru Shiotani <bruno_shiotani@outlook.com>
2024-03-06 15:32:47 -0300

    S7 - A53 - Uso de SWITCH/CASE no BLADE

--- app_super_gestao/resources/views/app/fornecedor/index.blade.php
+++ app_super_gestao/resources/views/app/fornecedor/index.blade.php
@@ -1,4 +1,4 @@
-<h3>Fornecedor</h3>
+<h3>Fornecedores</h3>
 
 
 @php
@@ -13,32 +13,34 @@
     - array[]
     - $var
     */
-    $const = 2;
 @endphp
 {{--@dd($fornecedores)--}}
 {{--UNLESS executa se o retorno for FALSE--}}
 
-
+<hr>
 @isset($fornecedores)
-    Fornecedor: {{$fornecedores[$const]['nome']}}
-    <br>
-    Status: {{$fornecedores[$const]['status']}}
-    <br>
-    CNPJ: {{$fornecedores[$const]['cnpj'] ?? 'Dado não foi preenchido'}}
-    <br>
-    Telefone: ({{$fornecedores[$const]['ddd'] ?? '00'}}) {{$fornecedores[$const]['telefone'] ?? ''}}
-    <br>
-    @switch($fornecedores[$const]['ddd'])
-        @case ('11')
-            São Paulo - SP
-            @break
-        @case ('85')
-            Fortaleza - CE 
-            @break
-        @case ('32')
-            Juiz de Fora - MG
-            @break
-        @default
-            Estado não idenetificado
-    @endswitch
+    @for($i = 0; isset($fornecedores[$i]); $i++)
+        Fornecedor: {{$fornecedores[$i]['nome']}}
+        <br>
+        Status: {{$fornecedores[$i]['status']}}
+        <br>
+        CNPJ: {{$fornecedores[$i]['cnpj'] ?? 'Dado não foi preenchido'}}
+        <br>
+        Telefone: ({{$fornecedores[$i]['ddd'] ?? '00'}}) {{$fornecedores[$i]['telefone'] ?? ''}}
+        <br>
+        @switch($fornecedores[$i]['ddd'])
+            @case ('11')
+                São Paulo - SP
+                @break
+            @case ('85')
+                Fortaleza - CE 
+                @break
+            @case ('32')
+                Juiz de Fora - MG
+                @break
+            @default
+                Estado não idenetificado
+        @endswitch
+        <hr>
+    @endfor
 @endisset
