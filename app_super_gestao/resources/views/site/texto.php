d9908d33865e80e336a2253d8cf280ce5302e145
Bruno Tohoru Shiotani <bruno_shiotani@outlook.com>
2024-03-07 08:51:52 -0300

    S7 - A72 - Criação de um MODEL utilizando o comando php artisan make:moldel nome_do_model -m, o -m é para criar um migration para passagem de dados para o banco de dados

---
+++ app_super_gestao/resources/views/site/texto.php
@@ -0,0 +1,18 @@
+tag {
+    text-aling: center;
+}
+
+.classe1 {
+    margin-top: 20px;
+    border: 10px solid #8db7d2;
+    border-radius: 50%;
+}
+
+.classe2 {
+    color: #747c81;
+    margin-top: 20px;
+}
+
+.classe3 {
+    margin-top: 10px;
+}
