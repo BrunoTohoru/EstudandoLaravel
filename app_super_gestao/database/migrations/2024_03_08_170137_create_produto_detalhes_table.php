cad38e25da1bede4ea2e1971e4b5c90a09c649a6
Bruno Shiotani <bruno_shiotani@outlook.com>
2024-03-08 14:29:19 -0300

    S8 - A86 - Criando uma tabela com relação 1:1 no MIGRATION

---
+++ app_super_gestao/database/migrations/2024_03_08_170137_create_produto_detalhes_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProdutoDetalhesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produto_detalhes', function (Blueprint $table) {
+            //colunas
+            $table->id();
+            $table->unsignedBigInteger('produto_id'); //singular do nome da table (produto) + o nome da coluna que guarda a chave primaria (_id)
+            $table->float('comprimento', 8,2);
+            $table->float('largura', 8,2);
+            $table->float('altura', 8,2);
+            $table->timestamps();
+
+            //constraint
+            $table->foreign('produto_id')->references('id')->on('produtos');
+            $table->unique('produto_id'); //sem o UNIQUE seria uma relação de 1:n
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produto_detalhes');
+    }
+}
