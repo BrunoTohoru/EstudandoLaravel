e844a5279075b527aa7a43b57d1561b2d735fbc5
Bruno Shiotani <bruno_shiotani@outlook.com>
2024-03-12 17:41:18 -0300

    S9 - A123 - Adicionando um registro no banco de dados utilizando o objeto REQUEST no LARAVEL

--- app_super_gestao/app/Http/Controllers/ContatoController.php
+++ app_super_gestao/app/Http/Controllers/ContatoController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\SiteContato;
 
 class ContatoController extends Controller
 {
     public function contato(Request $request){
+        /*
         echo '<pre>';
         print_r($request->all());
         echo '</pre>';
@@ -14,7 +16,24 @@ class ContatoController extends Controller
         echo '<br>';
         echo $request->input('email');
         echo '<br>';
+        */
+        
+        /*
+        $contato = new SiteContato();
+        $contato->nome = $request->input('nome');
+        $contato->telefone = $request->input('telefone');
+        $contato->email = $request->input('email');
+        $contato->motivo_contato = $request->input('motivo_contato');
+        $contato->mensagem = $request->input('mensagem');
+
+        //print_r($contato->getAttribute());
         //var_dump($_POST);
+        $contato->save();
+        */
+
+        $contato = new SiteContato();
+        $contato->create($request->all());
+        // $contato->save();
         return view('site.contato');
     }
 }
