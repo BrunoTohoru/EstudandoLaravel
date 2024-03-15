e3eed70ecaae86d242dd614cc81aae362c851264
Bruno Shiotani <bruno_shiotani@outlook.com>
2024-03-15 15:49:23 -0300

    S10 - A147 - Implementando o formulario de Logi

--- app_super_gestao/app/Http/Controllers/LoginController.php
+++ app_super_gestao/app/Http/Controllers/LoginController.php
@@ -11,7 +11,20 @@ class LoginController extends Controller
         return view('site.login', ['titulo' => 'login']);
     }
 
-    public function autenticar() {
-        return 'Chegamos até aqui';
+    public function autenticar(Request $request) {
+        $regras = [
+            'usuario' => 'email',
+            'senha' => 'required'
+        ];
+
+        // mensagens de validação
+        $feedback = [
+            'usuario.email' => 'O campo usuário (e-mail) é obrigatório',
+            'senha.required' => 'O campo sneha é obrigatório',
+        ];
+
+        $request->validate($regras, $feedback);
+        
+        print_r($request->all());
     }
 }
