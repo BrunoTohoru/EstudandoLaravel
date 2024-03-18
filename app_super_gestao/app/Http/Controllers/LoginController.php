ac0bb96a6caf050844fab322320be70ea37b7fcd
Bruno Shiotani <bruno_shiotani@outlook.com>
2024-03-18 14:48:05 -0300

    S10 - A149 a 153 - Adicionando sessões à aplicação e novos VIEWs

--- app_super_gestao/app/Http/Controllers/LoginController.php
+++ app_super_gestao/app/Http/Controllers/LoginController.php
@@ -3,12 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
 
 class LoginController extends Controller
 {
     //
-    public function index() {
-        return view('site.login', ['titulo' => 'login']);
+    public function index(Request $request) {
+
+        $erro = '';
+        
+        if ($request->get('erro') == 1) {
+            $erro = 'Usuario e ou senha não existem';
+        }
+        
+        if ($request->get('erro') == 2) {
+            $erro = 'Necessário realizar login para ter acesso à página';
+        }
+
+        return view('site.login', ['titulo' => 'login', 'erro' => $erro]);
     }
 
     public function autenticar(Request $request) {
@@ -20,11 +32,38 @@ class LoginController extends Controller
         // mensagens de validação
         $feedback = [
             'usuario.email' => 'O campo usuário (e-mail) é obrigatório',
-            'senha.required' => 'O campo sneha é obrigatório',
+            'senha.required' => 'O campo senha é obrigatório',
         ];
 
         $request->validate($regras, $feedback);
+
+        // recuperamos os parâmetros do formulário
+        $email = $request->get('usuario');
+        $senha = $request->get('senha');
         
-        print_r($request->all());
+        // iniciar o Model User
+        $user = new User();
+        $usuario = $user
+            ->where('email', $email)
+            ->where('password', $senha)
+            ->get()
+            ->first();
+        
+        if (isset($usuario->name)) {
+
+            session_start();
+            $_SESSION['nome'] = $usuario->name;
+            $_SESSION['email'] = $usuario->email;
+
+            return redirect()->route('app.home');
+        } else {
+            return redirect()->route('site.login', ['erro'=> 1]);
+        }
+        
+    }
+
+    public function sair() {
+        session_destroy();
+        return redirect()->route('site.index');
     }
 }
--- app_super_gestao/app/Http/Middleware/AutenticacaoMiddleware.php
+++ app_super_gestao/app/Http/Middleware/AutenticacaoMiddleware.php
@@ -15,6 +15,14 @@ class AutenticacaoMiddleware
      */
     public function handle($request, Closure $next, $metodo_autenticacao, $perfil)
     {
+        session_start();
+
+        if (isset($_SESSION['email']) && $_SESSION['email'] != '') {
+            return $next($request);
+        } else {
+            return redirect()->route('site.login', ['erro' => 2]);
+        }
+        /*
         // verificar usuário tem acesso a rota
         echo $metodo_autenticacao.' - '.$perfil.'<br>';
 
@@ -37,6 +45,7 @@ class AutenticacaoMiddleware
         } else {
             return response('Acesso negado! Rota exige autenticação!!!');
         }
+        */
 
     }
 }
