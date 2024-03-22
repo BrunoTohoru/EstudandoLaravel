5066f4b724902178963e0b5e9d199cc0414bb0ff
Bruno Shiotani <bruno_shiotani@outlook.com>
2024-03-22 15:21:46 -0300

    S11 - A172 a 173 - Componentização do formunlário para edição e criação de novos registros.

--- app_super_gestao/resources/views/app/produto/create.blade.php
+++ app_super_gestao/resources/views/app/produto/create.blade.php
@@ -1,13 +1,17 @@
 @extends('app.layouts.basico')
 
-@section('titulo', 'Produto - Adicionar')
+@section('titulo', 'Produto')
 
 @section('conteudo')
     
     <div class="conteudo-pagina">
 
         <div class="titulo-pagina-2">
-            <p>Produto - Adicionar</p>
+            @if(isset($produto->id))
+                <p>Produto - Editar</p>
+            @else
+                <p>Produto - Adicionar</p>
+            @endif
         </div>
 
         <div class="menu">
@@ -19,23 +23,8 @@
 
         <div class="informacao-pagina">
             <div style="width:30%; margin-left: auto; margin-right: auto;">
-                <form method="POST" action="{{ route('produto.store') }}">
-                    @csrf
-                    <input type="text" name="nome" value="{{ old('nome') }}" placeholder="Nome" class="borda-preta">
-                    {{ $errors->has('nome') ? $errors->first('nome') : '' }}
-                    <input type="text" name="descricao" value="{{ old('descricao') }}" placeholder="Descrição" class="borda-preta">
-                    {{ $errors->has('descricao') ? $errors->first('descricao') : '' }}
-                    <input type="text" name="peso" value="{{ old('peso') }}" placeholder="Peso" class="borda-preta">
-                    {{ $errors->has('peso') ? $errors->first('peso') : '' }}
-                    <select name="unidade_id" class="borda-preta">
-                        <option>-- Selecione a Unidade de Medida --</option>
-                        @foreach ($unidades as $unidade)
-                            <option value="{{ $unidade->id }}" {{ old('unidade_id') == $unidade->id ? 'selected' : '' }}>{{ $unidade->descricao }}</option>
-                        @endforeach
-                    </select>
-                    {{ $errors->has('unidade_id') ? $errors->first('unidade_id') : '' }}
-                    <button type="submit" class="borda-preta">Cadastrar</button>
-                </form>
+                @component('app.produto._components.form_create_edit', ['unidades'=> $unidades])
+                @endcomponent
             </div>
         </div>
         
