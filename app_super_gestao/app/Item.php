0e3d726204bf3f3075333ffb8120206a9183b39f
Bruno Shiotani <bruno_shiotani@outlook.com>
2024-03-25 14:36:25 -0300

    S11 - A184 a 188 - ELOQUENT ORM 1 para N com  HASMANY e BELONGSTO.

--- app_super_gestao/app/Item.php
+++ app_super_gestao/app/Item.php
@@ -7,9 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Item extends Model
 {
     protected $table = 'produtos';
-    protected $fillable = ['nome', 'descricao', 'peso', 'unidade_id'];
+    protected $fillable = ['nome', 'descricao', 'peso', 'unidade_id', 'fornecedor_id'];
 
     public function itemDetalhe() {
     return $this->hasOne('App\ItemDetalhe', 'produto_id'/*FK*/, 'id'/*PK*/);
+    }
+
+    public function fornecedor() {
+        return $this->belongsTo('App\Fornecedor', 'fornecedor_id', 'id');
     }
 }
--- app_super_gestao/resources/views/app/fornecedor/listar.blade.php
+++ app_super_gestao/resources/views/app/fornecedor/listar.blade.php
@@ -18,37 +18,57 @@
         </div>
 
         <div class="informacao-pagina">
-            <div style="width:30%; margin-left: auto; margin-right: auto;">
-                <form method="POST" action="">
-                    <table border="1" width="100%">
-                        <thead>
-                            <tr>
-                                <th>Nome</th>
-                                <th>Site</th>
-                                <th>UF</th>
-                                <th>E-mail</th>
-                                <th></th>
-                                <th></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach($fornecedores as $fornecedor)
-                                <tr>
-                                    <td>{{ $fornecedor->nome }}</td>
-                                    <td>{{ $fornecedor->site }}</td>
-                                    <td>{{ $fornecedor->uf }}</td>
-                                    <td>{{ $fornecedor->email }}</td>
-                                    <td><a href="{{ route('app.fornecedor.excluir', $fornecedor->id) }}">Excluir</a></td>
-                                    <td><a href="{{ route('app.fornecedor.editar', $fornecedor->id) }}">Editar</a></td>
-                                </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
+            <div style="width: 90%; margin-left: auto; margin-right: auto;">
+                <table border="1" width="100%">
+                    <thead>
+                        <tr>
+                            <th>Nome</th>
+                            <th>Site</th>
+                            <th>UF</th>
+                            <th>E-mail</th>
+                            <th></th>
+                            <th></th>
+                        </tr>
+                    </thead>
 
-                    {{ $fornecedores->appends($request)->links() }}
-                    <br>
-                    Exibindo {{ $fornecedores->count() }} fornecedores de {{ $fornecedores->total() }} (de {{ $fornecedores->firstItem()}} a {{ $fornecedores->lastItem()}} )
-                </form>
+                    <tbody>
+                        @foreach($fornecedores as $fornecedor)
+                            <tr>
+                                <td>{{ $fornecedor->nome }}</td>
+                                <td>{{ $fornecedor->site }}</td>
+                                <td>{{ $fornecedor->uf }}</td>
+                                <td>{{ $fornecedor->email }}</td>
+                                <td><a href="{{ route('app.fornecedor.excluir', $fornecedor->id) }}">Excluir</a></td>
+                                <td><a href="{{ route('app.fornecedor.editar', $fornecedor->id) }}">Editar</a></td>
+                            </tr>
+                            <tr>
+                                <td colspan="6">
+                                    <p>Lista de produtos</p>
+                                    <table border="1" style="margin-left:auto; margin-right:auto; margin-bottom:10px">
+                                        <thead>
+                                            <tr>
+                                                <th>ID</th>
+                                                <th>Nome</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody >
+                                            @foreach($fornecedor->item as $key => $produto)
+                                                <tr>
+                                                    <td>{{ $produto->id }}</td>
+                                                    <td>{{ $produto->nome }}</td>
+                                                </tr>
+                                            @endforeach
+                                        </tbody>
+                                    </table>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+
+                {{ $fornecedores->appends($request)->links() }}
+                <br>
+                Exibindo {{ $fornecedores->count() }} fornecedores de {{ $fornecedores->total() }} (de {{ $fornecedores->firstItem()}} a {{ $fornecedores->lastItem()}} )
             </div>
         </div>
         
