10e1b456a389fc74d3f48cc0e008927b71f7f66f
Bruno Shiotani <bruno_shiotani@outlook.com>
2024-04-15 11:00:45 -0300

    LC03 - Implementação do model e controller da marca

--- app_locadora_carros/routes/api.php
+++ app_locadora_carros/routes/api.php
@@ -19,8 +19,8 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 //Route::resource('cliente', 'App\Http\Controllers\ClienteController');
-Route::apiResource('cliente', 'App\Http\Controllers\ClienteController');
-Route::apiResource('carro', 'App\Http\Controllers\CarroController');
-Route::apiResource('locacao', 'App\Http\Controllers\LocacaoController');
-Route::apiResource('marca', 'App\Http\Controllers\MarcaController');
-Route::apiResource('modelo', 'App\Http\Controllers\ModeloController');
+Route::apiResource('cliente', 'ClienteController');
+Route::apiResource('carro', 'CarroController');
+Route::apiResource('locacao', 'LocacaoController');
+Route::apiResource('marca', 'MarcaController');
+Route::apiResource('modelo', 'ModeloController');
